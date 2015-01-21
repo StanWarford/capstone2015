afb3d9dff9948b5fea25c905df524ae419449b21
Chloe Cheung <chloencheung@gmail.com>
2015-01-20 19:54:48 -0800

    Initial home page
    Added files and css for initial few pages of the app

---
+++ Hello World/platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\Chloe\Documents\GitHub\capstone2015\Hello World\platforms\android\ant-build\AndroidManifest.xml : \
+C:\Users\Chloe\Documents\GitHub\capstone2015\Hello World\platforms\android\AndroidManifest.xml \
---
+++ Hello World/platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+C:\Users\Chloe\Documents\GitHub\capstone2015\Hello World\platforms\android\ant-build\CordovaApp-debug-unaligned.apk : \
+C:\Users\Chloe\Documents\GitHub\capstone2015\Hello World\platforms\android\ant-build\CordovaApp.ap_ \
+C:\Users\Chloe\Documents\GitHub\capstone2015\Hello World\platforms\android\ant-build\classes.dex \
+C:\Users\Chloe\Documents\GitHub\capstone2015\Hello World\platforms\android\CordovaLib\ant-build\classes.jar \
