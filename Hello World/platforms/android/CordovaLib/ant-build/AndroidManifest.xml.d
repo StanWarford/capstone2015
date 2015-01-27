7f61d3ea218b0d12ba50f80ffa2ac87f7c3174d3
Chloe Cheung <chloencheung@gmail.com>
2015-01-26 17:32:54 -0800

    Got rid of HelloWorld
    Goodbye HelloWorld! Left Sam's new version for us to work on from now
    on.

--- Hello World/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Users\Chloe\Documents\GitHub\capstone2015\Hello World\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
-C:\Users\Chloe\Documents\GitHub\capstone2015\Hello World\platforms\android\CordovaLib\AndroidManifest.xml \
--- Hello World/platforms/android/ant-build/classes.dex.d
+++
@@ -1,9 +0,0 @@
-C:\Users\Chloe\Documents\GitHub\capstone2015\Hello World\platforms\android\ant-build\classes.dex : \
-C:\Users\Chloe\Documents\GitHub\capstone2015\Hello World\platforms\android\ant-build\classes\com\phonegap\helloworld\BuildConfig.class \
-C:\Users\Chloe\Documents\GitHub\capstone2015\Hello World\platforms\android\ant-build\classes\com\phonegap\helloworld\CordovaApp.class \
-C:\Users\Chloe\Documents\GitHub\capstone2015\Hello World\platforms\android\ant-build\classes\com\phonegap\helloworld\R$attr.class \
-C:\Users\Chloe\Documents\GitHub\capstone2015\Hello World\platforms\android\ant-build\classes\com\phonegap\helloworld\R$drawable.class \
-C:\Users\Chloe\Documents\GitHub\capstone2015\Hello World\platforms\android\ant-build\classes\com\phonegap\helloworld\R$string.class \
-C:\Users\Chloe\Documents\GitHub\capstone2015\Hello World\platforms\android\ant-build\classes\com\phonegap\helloworld\R$xml.class \
-C:\Users\Chloe\Documents\GitHub\capstone2015\Hello World\platforms\android\ant-build\classes\com\phonegap\helloworld\R.class \
-C:\Users\Chloe\Documents\GitHub\capstone2015\Hello World\platforms\android\ant-build\dexedLibs\classes-0f6dde9fe53cd7b19621c37f29ad2adc.jar \
