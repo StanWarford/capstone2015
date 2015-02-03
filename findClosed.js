916b5dcf1997f1c4961ca08274e13f2f90e09660
Adam Inglehart <adaminglehart@cupps-Mac-Pro-2.local>
2015-02-02 17:10:32 -0800

    Added findClosed.js
    Compares two databases and collects all classes that have changed
    status or been cancelled

--- findClosed.js
+++ findClosed.js
@@ -1,48 +1,15 @@
-var json1 = {
-	//Class
-	{
-		"ID" : "Big number",
-		"section" : "COSC105.01",
-		"info" : {
-			"room" : "RAC170",
-			"status" : "open",
-			"professor" : "Brad Cupp"	
-		},
-		"users" : {
-			"1" : "Token1",
-			"2" : "Token2"
-		}
-	}
-	// Repeat 
-}
-
-var json2 = {
-	//Class
-	{
-		"ID" : "Big number",
-		"section" : "COSC105.01",
-		"info" : {
-			"room" : "RAC170",
-			"status" : "closed",
-			"professor" : "Brad Cupp"	
-		},
-		"users" : {
-			"1" : "Token1",
-			"2" : "Token2"
-		}
-	}
-	// Repeat 
-}
-
 var changed = {};
 
-function compare(db_old, db_new){
-	for (var section in db_old){
-		if (db_new[section]["info"]["status"] == null){
+for (var entry in db_old){
+	var match = db_new.find(checkSection, db_old[entry]);
+  	if (!match){
+    	changed[db_old[entry]["section"]] = "cancelled";
+  	}
+   	else if (match["info"]["status"] != db_old[entry]["info"]["status"]){
+        changed[match["section"]] = match["info"]["status"];
+    }
+}
 
-		}
-		else if (db_old[section]["info"]["status"] != db_new[section]["info"][status]){
-			changed[section] = db_new["info"]["status"];
-		}
-	}
+function checkSection(element, index, array){
+	return element["section"] == this["section"];
 }
