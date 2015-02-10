924def1ac8fc787d14770d3e27c2cb29f5193bdc
Jeremiah Montoya <jeremiahmontoya@perlis.local>
2015-02-10 15:18:46 -0800

    Fixed the exit buttons of three other UIViewControllers to dismiss views.

---
+++ capstone2015/capstone2015/SettingsViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SettingsViewController.swift
+//  capstone2015
+//
+//  Created by Jeremiah Montoya on 2/10/15.
+//  Copyright (c) 2015 Pepperdine Computer Science. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBAction func close(sender: UIBarButtonItem) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
