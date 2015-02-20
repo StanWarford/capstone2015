58215418afd63bfef29f90885927599d0bd3a5fa
SamOlson <dolphingirl_40@Samantha-Olsons-MacBook-Pro.local>
2015-02-19 18:10:52 -0800

    Deleted "finish" folder
    All of the finish files were moved to capstone folder. Finish was
    deleted to avoid redundancy

--- finish/www/js/head.js
+++
@@ -1,7 +0,0 @@
-document.write{'<p>Hello!</p>'};
-	
-	/*'\
-<a href="#mypanel" class="ui-btn ui-corner-all ui-shadow ui-btn-inline ui-icon-bars ui-btn-icon-left ui-btn-a menu">menu</a>\
-<h1>Page Title</h1>\
-<a href="index.html" class="ui-btn ui-corner-all ui-shadow ui-btn-inline ui-icon-home ui-btn-icon-right ui-btn-a menu">home</a>\
-'};*/
--- finish/www/js/index.js
+++
@@ -1,2 +0,0 @@
-var x = document.getElementById("cal-table").rows[1].cells;
-x[2].innerHTML = "NEW CONTENT";
