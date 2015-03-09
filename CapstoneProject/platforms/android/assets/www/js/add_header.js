7053b6bb883f498e94386c6e72b86f01fb0eb833
Chloe Cheung <chloencheung@gmail.com>
2015-03-09 16:43:05 -0700

    Committing so I can Pull..
    Committing arbitrary push notification things; I'm going to commit so I
    can pull and then keep trying at getting our app to build with the push
    notification plugin

--- CapstoneProject/platforms/android/assets/www/js/add_header.js
+++
@@ -1,7 +0,0 @@
-function add_header () {
-	console.log("loaded");
-	var header_contents = read_contents ("header.txt" );
-	console.log("header" + header_contents);
-	$("#header").html(header_contents);
-	alert("done");
-}
